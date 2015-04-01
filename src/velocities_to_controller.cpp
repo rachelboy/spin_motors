0ded8d4afe45d19a6f8a06357c7d843fa8621352
rachelboy <rachel.boy@gmail.com>
2015-04-01 12:50:59 -0400

    velocities to controller appears to work, huzzah

--- src/velocities_to_controller.cpp
+++ src/velocities_to_controller.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #include "RoboteqDevice.h"
 #include "ErrorCodes.h"
@@ -18,6 +19,11 @@ const int LEFT = 2;
 RoboteqDevice device;
 int status;
 string response;
+
+void rightCallback(const std_msgs::Float32::ConstPtr&);
+void leftCallback(const std_msgs::Float32::ConstPtr&);
+int velToCmd(float);
+void setConfig(int, int, int);
 
 
 void rightCallback(const std_msgs::Float32::ConstPtr& msg) {
@@ -45,7 +51,7 @@ void leftCallback(const std_msgs::Float32::ConstPtr& msg) {
     cout<<"failed --> "<<status<<endl;
   else
     cout<<"succeeded."<<endl;
-})
+}
 
 int velToCmd(float vel) {
   // 0.1524 m is the diameter of the wheel
@@ -53,7 +59,8 @@ int velToCmd(float vel) {
   float wheel_circumfrence = 3.1415926535*0.1524;
   float wheel_rpm = vel*60/wheel_circumfrence;
   float motor_rpm = wheel_rpm*10;
-  float per_cmd = motor_rpm/max_motor_rpm;
+  // make sure we don't go over 100% or under -100%
+  float per_cmd = fmax(fmin(motor_rpm/max_motor_rpm,1.0),-1.0);
   int cmd = int(per_cmd*1000);
   return cmd;
 }
